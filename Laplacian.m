4ab75eed1030133d77b6f93234b0900c4c6e7a51
Ben Olson <bpaulolson@gmail.com>
2025-07-21 12:53:49 -0600

    Laplacian test, with random walk

---
+++ Laplacian.m
@@ -0,0 +1,34 @@
+% Laplacian with weighted edges and loops at node 1 2 4 and 5 of edge
+% weights 1
+
+
+%    .8
+% 2 ---- 3 
+% |      | \.9
+% |.2 .7 |   5
+% |      | /.1
+% 1 ---- 4
+%    .5
+
+% adjacent matrix inlcudes weight and loops
+A = [1 .2 0 .5 0;
+     .2 1 .8 0 0;
+     0 .8 0 .7 .9;
+     .5 0 .7 1 .1;
+     0 0 .9 .1 1];
+
+% degree matrix
+D = diag(sum(A));
+
+L = D - A
+
+%randow walk
+
+I = eye(5);
+
+
+L_rw = I - inv(D)*A
+
+
+
+

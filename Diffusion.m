2fcf600d6f55c46c79b62c28312becf99574c684
Ben Olson <bpaulolson@gmail.com>
2025-08-14 15:49:22 -0600

    Diffusion

--- Diffusion.m
+++ Diffusion.m
@@ -1,4 +1,4 @@
-
+ 
 
 % Laplacian with weighted edges
 
@@ -12,23 +12,24 @@
 %    .5
 
 % adjacent matrix includes weight and loops
-A = [0 .2 0 .5 0;
+A = [1 .2 0 .5 0;
      .2 0 .8 0 0;
-     0 .8 0 .7 .9;
+     0 .8 1 .7 .9;
      .5 0 .7 0 .1;
-     0 0 .9 .1 0];
+     0 0 .9 .1 1];
 
 % degree matrix
 D = diag(sum(A));
 
 %Laplacian
-
 L = D - A;
 disp("laplacian Matrix:")
 disp(L)
 
+
 %eignevalues and vectors
 [V,D] = eig(L);
+
 
 disp('diagonal matrix of eigenvalues:');
 disp(diag(D));

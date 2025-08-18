04705185bb9a3815e99706f1e3228e25928c1bb3
Ben Olson <bpaulolson@gmail.com>
2025-08-17 20:16:56 -0600

    Updated Laplacian

--- Diffusion.m
+++ Diffusion.m
@@ -1,7 +1,4 @@
- 
-
 % Laplacian with weighted edges
-
 
 %    .8
 % 2 ---- 3 
@@ -12,27 +9,31 @@
 %    .5
 
 % adjacent matrix includes weight and loops
-A = [1 .2 0 .5 0;
-     .2 0 .8 0 0;
-     0 .8 1 .7 .9;
-     .5 0 .7 0 .1;
-     0 0 .9 .1 1];
+x_11 = 0;
+x_22 = 1;
+x_33 = 1;
+x_44 = 1;
+x_55 = 1;
 
+A = [x_11 .2 0 .5 0;
+     .2 x_22 .8 0 0;
+      0 .8 x_33 .7 .9;
+     .5 0 .7 x_44 .1;
+      0 0 .9 .1 x_55];
 % degree matrix
 D = diag(sum(A));
 
 %Laplacian
-L = D - A;
-disp("laplacian Matrix:")
-disp(L)
 
+D_invsqrt = diag(1 ./ sqrt(diag(D)));
+L =  eye(5) - D_invsqrt*A*D_invsqrt;
 
 %eignevalues and vectors
-[V,D] = eig(L);
+[V,E] = eig(L);
 
 
 disp('diagonal matrix of eigenvalues:');
-disp(diag(D));
+disp(diag(E));
 
 disp('Eigenvectors:');
 disp(V);
@@ -53,14 +54,14 @@ a(j) = dot(v_j, u0)/norm(v_j)^2;
 
 end
 
-t = linspace(0,1,100);
+t = linspace(0,5,100);
 u_t = zeros(n,length(t));
 
 for j = 1:length(t)
     u_sum = zeros(n,1);  
     
     for k = 1:n
-        lambda_k = D(k,k);
+        lambda_k = E(k,k);
         v_k = V(:,k);
         u_sum = u_sum + a(k) * exp(-lambda_k * t(j)) * v_k;
     end
